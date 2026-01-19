51273388ec4d748eb8793b7ba8bd35401a765e09
Henri Podolski <henrip@gmail.com>
2026-01-19 16:49:49 +0100

    feat: scaffold presentational ui

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-export default function main() {}
+function main() {
+    const modules = import.meta.glob('./components/**/index.html');
+
+    // Extract folder names from the keys
+    const componentFolders = Object.keys(modules).map((path) => {
+        // Transform "./components/accordion/index.html" -> "accordion"
+        return path.split('/')[2];
+    });
+
+    console.log('Available components:', componentFolders);
+}
+
+
+main();
