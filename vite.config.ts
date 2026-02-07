adcfbb75f581985f1fd9d84684962ec18ce0f204
Henri Podolski <henrip@gmail.com>
2026-02-07 20:26:05 +0100

    feat: Add and configure vite-plugin-static-copy to copy HTML components.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig(({ command }) => ({
   base: '/patterns/',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/components/**/*.html',
+          dest: 'src/components',
+        }
+      ]
+    })
+  ]
 }))
